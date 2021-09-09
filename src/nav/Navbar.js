2e78ae8b96858d1ee553d8b40aaeb43af8ba4ebf
Bryant Richards <dragonseyeimages@gmail.com>
2021-09-09 11:36:12 -0500

    side nav finish

--- src/nav/Navbar.js
+++ src/nav/Navbar.js
@@ -1,27 +1,24 @@
 import React, { Fragment } from 'react'
-import { FaHome } from 'react-icons/fa'
+import { FaChevronLeft } from 'react-icons/fa'
 import './styles.css'
 
 const Navbar = () => {
   return (
     <Fragment>
-      <input type="checkbox" name="" id="side-menu-switch" />
-    <div className="side-menu">
+      <input type='checkbox' name='' id='side-menu-switch' checked='false' />
+    <div className='side-menu'>
           <nav>
-            <div className='link-container'>
-              <a href="#">Home</a>
-            <span><FaHome /></span>
-            </div>
-            <a href="#">Account</a>
-            <a href="#">Profile</a>
-            <a href="#">My Links</a>
-            <a href="#">Chat</a>
+            <a href='#'>Home</a>
+            <a href='#'>Account</a>
+            <a href='#'>Profile</a>
+            <a href='#'>My Links</a>
+            <a href='#'>Chat</a>
           </nav>
           <nav className='bottom-nav'>
-            <a href="#">Login/Out</a>
+            <a href='#'>Login/Out</a>
           </nav>
-          <label for="side-menu-switch">
-
+          <label for='side-menu-switch'>
+            <FaChevronLeft className='icon' />
           </label>
         </div>
     </Fragment>
